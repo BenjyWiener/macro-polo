a4dd43981364956a7ccf20a867fbc5b21832220b
Benjy Wiener <info@BenjyWiener.com>
2025-03-26 13:03:13 +0200

    Add `debug` predefined macro

--- src/macro_polo/macros/predefined.py
+++ src/macro_polo/macros/predefined.py
@@ -1,6 +1,7 @@
 """Predefined macros."""
 
 from collections.abc import Sequence
+import sys
 import tokenize
 
 from .. import Token, stringify
@@ -15,11 +16,22 @@ from . import (
 
 def stringify_macro(tokens: Sequence[Token]) -> Sequence[Token] | None:
     """Render tokens to a string of source code."""
+    if len(tokens) == 1:
+        # Special case for single tokens, avoids extraneous white space
+        return (Token(tokenize.STRING, repr(tokens[0].string)),)
+
     return (Token(tokenize.STRING, repr(stringify(tokens))),)
+
+
+def debug_macro(tokens: Sequence[Token]) -> Sequence[Token] | None:
+    """Stringify and print `tokens` to stderr during macro expansion."""
+    print(stringify(tokens), file=sys.stderr)
+    return ()
 
 
 DEFAULT_NAMED_MACROS = {
     'stringify': stringify_macro,
+    'debug': debug_macro,
 }
 
 
